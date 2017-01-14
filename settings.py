c9229ca1909bca9ccf456aebad481e9e3dc9b676
BrokenClock <dumboyadvance@gmail.com>
2017-01-14 10:18:47 +0100

    Create settings.py
    config file for jarvis

---
+++ settings.py
@@ -0,0 +1,13 @@
+def token():
+	return "262106974:AAHqTeiBBJldrBF7rE8U42Id2t7XIEw2koY"
+          123456789:ABCDEFGHIJKLMNOPQRSTUVWXYZAZERTYUIO
+
+
+#currently no idea how offsets work, so I might as well put that here. We'll see
+def offset():
+	return 10
+
+def chats(id):
+	if id == "chat_nickname"
+		id = -1001342424242	
+	return id
