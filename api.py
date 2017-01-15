b5d7d24cab7182e198e6d6e3cfa96945fc63a32c
BrokenClock <dumboyadvance@gmail.com>
2017-01-15 14:25:05 +0100

    Added api.py + Reorganised functions.

---
+++ api.py
@@ -0,0 +1,18 @@
+import urllib.request #needed to make request over the internet
+import json #needed to parse the json files returned by telegram
+import urllib.parse #allows to make http request with diverse characters
+import cust #my custom functions
+import settings #different bot settings, and onetime used variables
+import api
+
+def SendMessage(id,text):#self explanatory, ain't it?
+	Url=(ApiUrl+"sendMessage?chat_id="+str(id)+"&text="+str(text))
+	cust.proceed(Url)
+
+def SendChatAction(id,action):#check telegram's bot api documentation for inputable
+	Url=(ApiUrl+"sendChatAction?chat_id="+str(id)+"&action="+str(action))
+	cust.proceed(Url)
+
+def SendKeyboard(id, text, button, data):#just a simple, basic keyboard. I might make it better by creating a function that would create the json object before, it would allow me to make multi buttons messages. In addition, I still need to figure out how callback data works. I really suck at this.
+	Url=(ApiUrl+"sendMessage?chat_id="+str(id)+"&text="+str2http(text)+"""&reply_markup={%22inline_keyboard%22:[[{"text":%22"""+str2http(button)+"""","callback_data":%22"""+data+"}]]}")
+	cust.proceed(Url)
--- settings.py
+++ settings.py
@@ -1,13 +1,18 @@
 def token():
 	return "262106974:AAHqTeiBBJldrBF7rE8U42Id2t7XIEw2koY"
-          123456789:ABCDEFGHIJKLMNOPQRSTUVWXYZAZERTYUIO
 
-
-#currently no idea how offsets work, so I might as well put that here. We'll see
 def offset():
 	return 10
 
 def chats(id):
-	if id == "chat_nickname"
-		id = -1001342424242	
+	if id == "test":
+		id = -177486285
+	if id == "friends":
+		id = -1001045897759
+
+
+
+
+
+
 	return id
